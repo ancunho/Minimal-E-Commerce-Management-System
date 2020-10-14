2bbb8acc004ed46081cec1f36911c7929655ca74
cunho <ancunho@naver.com>
2020-10-15 01:52:53 +0800

    20201015

--- src/main/java/com/ahnstudio/management/controller/portal/OrderController.java
+++ src/main/java/com/ahnstudio/management/controller/portal/OrderController.java
@@ -5,10 +5,7 @@ import com.ahnstudio.management.service.OrderService;
 import com.ahnstudio.management.vo.CartVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 
@@ -24,7 +21,7 @@ public class OrderController {
     @Autowired
     private OrderService orderService;
 
-    @RequestMapping(value = "create")
+    @RequestMapping(value = "create", method = RequestMethod.POST)
     public ServerResponse create_order_no(HttpSession session, @RequestBody CartVO cartVO) {
         // 파라미터로 넘긴 장바구니정보로 주문번호 생성한다
         return orderService.createOrder(cartVO);
